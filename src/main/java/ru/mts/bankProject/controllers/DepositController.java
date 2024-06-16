3e3fb81abd492cf7e40984a4ba35cc96b035375d
Михаил <mberminov@yandex.ru>
2024-06-16 03:37:55 +0300

    deposits form added

--- src/main/java/ru/mts/bankProject/controllers/DepositController.java
+++ src/main/java/ru/mts/bankProject/controllers/DepositController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.mts.bankProject.services.DepositService;
+import ru.mts.bankProject.store.entities.CustomerEntity;
 import ru.mts.bankProject.store.entities.DepositEntity;
 
 import java.util.List;
@@ -23,24 +24,12 @@ public class DepositController {
         List<DepositEntity> deposits = depositService.getCustomerDeposits(customerId);
         System.out.println(deposits);
         model.addAttribute("deposits", deposits);
+        model.addAttribute("customerId", customerId);
         return "depositsList";
     }
 
-//    @GetMapping("/deposit/{id}")
-//    public String depositDetails(@PathVariable Long id, Model model) {
-//        DepositEntity deposit = depositService.getDepositById(id);
-//        model.addAttribute("deposit", deposit);
-//        return "deposit";
-//    }
-
-//    @PostMapping("/account/openDeposit")
-//    public String openDeposit(@RequestParam Long accountId, @RequestParam String depositNumber, @RequestParam BigDecimal amount, RedirectAttributes redirectAttributes) {
-//        try {
-//            depositService.openDeposit(accountId, depositNumber, amount);
-//            redirectAttributes.addFlashAttribute("message", "Вклад успешно открыт");
-//        } catch (Exception e) {
-//            redirectAttributes.addFlashAttribute("error", e.getMessage());
-//        }
-//        return "redirect:/account/" + accountId;
-//    }
+    @GetMapping("/customers/{customerId}/deposits/open")
+    public String userInfo(){
+        return "openDeposit";
+    }
 }
